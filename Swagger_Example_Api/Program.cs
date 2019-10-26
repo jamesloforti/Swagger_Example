f852eacf7b51fdb97aaa861bb344a36fd8cc9864
James LoForti <jamesloforti@gmail.com>
2019-10-26 16:26:09 -0600

    Added readme and licensing.

--- Swagger_Example_Api/Program.cs
+++ Swagger_Example_Api/Program.cs
@@ -1,10 +1,12 @@
 ﻿// ******************************************************************************************************************
+//  This file is part of Swagger_Example.
+//
 //  Swagger_Example - simple example to demonstrate swagger UI.
 //  Copyright(C) 2019  James LoForti
 //  Contact Info: jamesloforti@gmail.com
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
+//  Swagger_Example is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
@@ -13,7 +15,7 @@
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
+//  You should have received a copy of the GNU Affero General Public License
 //  along with this program.If not, see<https://www.gnu.org/licenses/>.
 //									     ____.           .____             _____  _______   
 //									    |    |           |    |    ____   /  |  | \   _  \  
